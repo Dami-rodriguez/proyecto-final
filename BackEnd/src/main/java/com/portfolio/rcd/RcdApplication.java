b7bc17f2551e19564901bb6aaa0aaf6d3a4d0adb
Dami-rodriguez <damianrdriguez46@gmail.com>
2022-05-28 21:43:58 -0300

    backEnd

---
+++ BackEnd/src/main/java/com/portfolio/rcd/RcdApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.rcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcdApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/main/java/com/portfolio/rcd/Service/ImpPeronaService.java
@@ -0,0 +1,36 @@
+package com.portfolio.rcd.Service;
+
+import com.portfolio.rcd.Entity.Persona;
+import com.portfolio.rcd.Interface.IPersonaService;
+import com.portfolio.rcd.Repository.IPersonaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImpPeronaService implements IPersonaService {
+    @Autowired IPersonaRepository ipersonaRepository;
+            
+    @Override
+    public List<Persona> getPersona() {
+        List<Persona> persona = ipersonaRepository.findAll();
+        return persona;
+    }
+
+    @Override
+    public void savePersona(Persona persona) {
+         ipersonaRepository.save(persona);
+    }
+
+    @Override
+    public void deletePersona(Long id) {
+        ipersonaRepository.deleteById(id);
+    }
+
+    @Override
+    public Persona findPersona(Long id) {
+        Persona persona = ipersonaRepository.findById(id).orElse(null);
+        return persona;
+    }
+    
+}
